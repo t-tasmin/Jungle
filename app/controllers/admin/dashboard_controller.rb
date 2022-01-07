b5fbe8f70726bb9763ca120cc853631edf7299f2
Tania Tasmin <taniat.kaau@email.com>
2022-01-06 20:35:07 -0500

    Count no of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
   def show
+    @product_count = Product.count
+
+    @category_count = Product.group(:category_id).count.count
   end
 end
